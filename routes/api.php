0c4e0adc2416df68b0db7d338c1ac1f0a46fd0a8
demfez <demfezeldar@gmail.com>
2021-01-05 16:33:49 +0200

    jwt & storage gitignore

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,26 @@
 <?php
 
+use App\Http\Middleware\Authenticate;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+//JWT Default routes
+Route::group([
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+    'prefix' => 'auth'
+
+], function ($router) {
+
+    Route::post('login', [AuthController::class, 'login'])->name('login');
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::post('refresh', [AuthController::class, 'refresh']);
+    Route::post('me', [AuthController::class, 'me']);
+
 });
+
+Route::get('abc', function(){
+
+    return response()->json(['message' => 'Authenticated']);
+
+})->middleware(Authenticate::class);
